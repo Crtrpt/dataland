090966614bed0bbc4b85dd8323ea583f58064273
jzx <945627077@qq.com>
2021-06-09 11:01:06 +0800

    '更新'

---
+++ src/main/java/com/dataland/Main.java
@@ -0,0 +1,13 @@
+package com.dataland;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Admin
+ */
+@Slf4j
+public class Main {
+    public static void main(String[] argv){
+        log.info("启动dataland");
+    }
+}
